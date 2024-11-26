5960465a633c76b0f6ffffabb5b97b8b20db0d55
Joel Lijo Mathew <joellijo4@gmail.com>
2024-11-26 07:21:31 +0530

    solved the . seperating problem by intialising the array to int at the first place.It was the Problem of Numpy to initialize using float type when used append() funct

--- 17 create,append,remove list using numpy.py
+++ 17 create,append,remove list using numpy.py
@@ -2,34 +2,36 @@ import numpy as np
 print("Enter the size of the List: ",end="")
 size = int(input())
 print(f"Enter {size} elements: ")
-list_of_elements =np.array([])
+list_of_elements =np.array([], dtype=int)
 for i in range(size): 
     value = int(input())
-    list_of_elements = np.append(list_of_elements, value)
+    list_of_elements = np.append(list_of_elements, int(value))
     
 print("The List Entered: ")
-print(list(list_of_elements))
+print( np.array2string(list_of_elements,separator=", "))
 
 
 
 print("Enter the numbers of elements to be appended: ",end="")
 size_of_elements_to_append  = int(input())
-print("Enter the Elements to be appended: ")
-for i in range(size_of_elements_to_append): 
-    value = int(input())
-    list_of_elements = np.append(list_of_elements,value)
+if size_of_elements_to_append > 0 :
+    print("Enter the Elements to be appended: ")
+    for i in range(size_of_elements_to_append): 
+        value = int(input())
+        list_of_elements = np.append(list_of_elements,value)
 
-print("Appended List: ")
-print(list(list_of_elements))
+    print("Appended List: ")
+    print(np.array2string(list_of_elements,separator=", "))
 
-print("Enter the number of elements to be Removed: ")
+print("Enter the number of elements to be Removed: ",end="")
 number_of_elements_to_remove = int(input())
-print("Enter the Elements to remove: ")
-for i in range(number_of_elements_to_remove):
-    value = int(input())
-    index = np.where(list_of_elements == value)
-    list_of_elements = np.delete(list_of_elements,index)
-    print("Removed :",value)
+if number_of_elements_to_remove > 0 : 
+    print("Enter the Elements to remove: ")
+    for i in range(number_of_elements_to_remove):
+        value = int(input())
+        index = np.where(list_of_elements == value)
+        list_of_elements = np.delete(list_of_elements,index)
+        print("Removed :",value)
 
-print("List after removing elements: ")
-print(list(list_of_elements))
+    print("List after removing elements: ")
+    print(np.array2string(list_of_elements,separator=", "))
