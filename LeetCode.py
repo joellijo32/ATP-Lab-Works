fd6eeb17e68aeec581926335cb5177d87576245c
Joel Lijo Mathew <joellijo4@gmail.com>
2024-12-05 07:26:28 +0530

    Completed halfway through Q26 and Leaving Behind due to time Constraint

--- LeetCode.py
+++ LeetCode.py
@@ -1,1 +1,7 @@
-print("Joel".find("q"))
+telphone_directory = {"Joel" : 8590453367,"Joshu" : 8281426957}
+def delete_contact(): 
+    print("\nEnter the Name of the Contact to Delete: ",end="")
+    name = input()
+    print(telphone_directory.pop(name,f"{name} Not Found in the Directory."))
+    print(telphone_directory)
+delete_contact()
