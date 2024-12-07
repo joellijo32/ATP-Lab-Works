527fe354b6c90d081e927c21053bb45d1c81881d
Joel Lijo Mathew <joellijo4@gmail.com>
2024-12-07 07:26:57 +0530

    Completed Q26: A menu driven program managing a Telephone Directory

--- 26TelephoneDirectory.py
+++ 26TelephoneDirectory.py
@@ -14,27 +14,61 @@ while True:
     # Adding
 
     if choice == 1: 
-        print("\nEnter the Name of the Contact: ",end="")
+        print("\n\nEnter the Name of the Contact: ",end="")
         name = input()
-        print("\nEnter the Number: ",end="")
+        print("Enter the Number: ",end="")
         number = int(input())
         telephone_directory[name] = number
-        print("\nThe Contact has been Succesfully Added.")
+        print("\nThe Contact has been Successfully Added.")
     
     # Updating 
 
     elif choice == 2: 
-        print("\nEnter the Name of the Contact to Udate: ",end="")
+        print("\n\nEnter the Name of the Contact to Udate: ",end="")
         name = input()
         if name not in telephone_directory: 
-            print(f"{name} is Not in the Directory")
+            print(f"{name} is Not in the Directory. Updation Failed .")
             continue
         else: 
             print("Enter the Number to Update: ",end="")
             number = int(input())
             telephone_directory.update({name : number})
-            print("\nThe Number has been Updated Succesfully.")
+            print("\nThe Number has been Updated Successfully.")
     
     # Deleting
 
-    pass
+    elif choice == 3 : 
+        name = input("\n\nEnter the Name of the Contact to Delete: ")
+        if name not in telephone_directory: 
+            print(f"{name} Not Found in the Directory. Deletion Failed.")
+        else: 
+            telephone_directory.pop(name)
+            print("The Contact has been Deleted Successfully.")
+    
+    # Searching
+
+    elif choice == 4: 
+        name = input("\n\nEnter the Name to Search: ")
+        if name not in telephone_directory : 
+            print(f"{name} Not Found in the Directory.")
+        else: 
+            print("Contact Found : ")
+            print(name , ":", telephone_directory[name])
+    
+    # Displaying
+
+    elif choice == 5: 
+        print("\n\nDisplaying All the Contacts: \n")
+        for i in telephone_directory: 
+            print(i,":",telephone_directory[i])
+        print("\nEnd of the List.")
+
+    # Exit 
+
+    elif choice == 6: 
+        print("\n\nExiting the Program.\n")
+        break
+
+
+
+    
--- LeetCode.py
+++ LeetCode.py
@@ -1,7 +1,5 @@
 telphone_directory = {"Joel" : 8590453367,"Joshu" : 8281426957}
 def delete_contact(): 
-    print("\nEnter the Name of the Contact to Delete: ",end="")
-    name = input()
-    print(telphone_directory.pop(name,f"{name} Not Found in the Directory."))
-    print(telphone_directory)
+    print(i for i in range(9))
+    
 delete_contact()
